6997c57edf46d9b681a50674d11682ba5d75a6c3
balakrishna88 <j.balakrishna88@gmail.com>
2022-11-04 23:51:03 +0530

    first commit

---
+++ applications/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from applications import views
+
+urlpatterns = [
+    path('',views.index, name="applications-index"),
+    path('/cat/<slug>/',views.applicationsReadCat, name="applicationsblog-cat"),
+    path('readBlog/<slug>/',views.applicationsReadBlog, name="applicationsreadblog"),
+]
---
+++ website/views.py
@@ -0,0 +1,5 @@
+from urllib import request
+from django.shortcuts import render
+
+def Home(request):
+    return render(request, 'website/index.html')
